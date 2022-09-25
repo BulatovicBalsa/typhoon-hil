72e0e9d202779960045589054310d3fb482a3968
Balša Bulatović <63477276+BulatovicBalsa@users.noreply.github.com>
2022-09-25 13:54:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import os
 import codecs
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'),
                      'r', encoding='utf-8').read()
 
 setup(
     name="graph_validations",
-    version='0.1',
-    description='Validations For Schematic Editor Simple Graphs',
+    version='0.2',
+    description='Validations And Transformations For Schematic Editor Simple Graphs',
+    packages=find_packages(),
     long_description=README,
     long_description_content_type='text/markdown',
     keywords="schematic editor validations",
-    install_requires=['Click'],
+    install_requires=['Click', "Typhoon-HIL-API"],
     license='MIT',
     author='Balsa Bulatovic',
     author_email='baki.b.2k02@gmail.com',
